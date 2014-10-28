bd472db77af86fa50c6df5b3860e8ee928307f6c
srli <sophia.li@students.olin.edu>
2014-10-28 13:59:40 -0400

    cleaned up

--- src/followcolor.cpp
+++ src/followcolor.cpp
@@ -41,61 +41,10 @@ int main(int argc,char *argv[])
         cvtColor(src, imgHSV, CV_BGR2HSV);
         inRange(imgHSV, Scalar(60, 70, 70), Scalar(120, 255, 255), imgPalm);
         inRange(imgHSV, Scalar(60, 70, 70), Scalar(120, 255, 255), imgPalm);
-        
+
         // Reduce the noise so we avoid false circle detection
         GaussianBlur( imgThreshed, gaussian_result, Size(9, 9), 2, 2 );
-     /*   vector<Vec3f> circles;
-        //std::vector<int> circles_radius;
-        CvSize dim = cvGetSize(color_img);
-        Point center_screen(dim.width/2,dim.height/2);
-        
-        //Draw center point of screen
-        circle(src, center_screen, 3, Scalar(255,0,0), -1, 8, 0);
-        circle(src, center_screen, 50, Scalar(255,0,0), 1, 8, 0);
-
-        // Apply the Hough Transform to find the circles
-        HoughCircles(gaussian_result, circles, CV_HOUGH_GRADIENT, 1, 30, 200, 30, 20, 0 );
-        if (circles.size() != 0){
-            
-            // Draw the circles detected
-            //for( size_t i = 0; i < circles.size(); i++ )
-            for( size_t i = 0; i < circles.size(); i++ ){
-                Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-                int radius = cvRound(circles[i][2]);
-
-                //Drawing each circle
-                circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );//center
-                circle( src, center, radius, Scalar(0,0,255), 3, 8, 0 );//circumference
-
-                //Drawing lines relative to center
-                Point midpoint(center.x, center_screen.y);
-                line(src, center_screen, center, Scalar(255,0,0), 1, 8, 0);//center to center
-                line(src, center_screen, midpoint, Scalar(255,0,0), 1, 8, 0);//y
-                line(src, midpoint, center, Scalar(255,0,0), 1, 8, 0);//x
-                
-                //cout << "center : " << center << "\nradius : " << radius << endl;
-                
-                double x_distance = center.x - center_screen.x;
-                double y_distance = center.y - center_screen.y;
-
-                double distance = 126.964*exp(-0.0216358 * radius);
-                double pan_angle = tan(x_distance/distance);
-                double tilt_angle = tan(y_distance/distance);
-
-                printf("");
-
-                key = cvWaitKey(100);
-*/
-              //}
-        //}
-    //}
-
-/*        else{
-            break;
-        }
-           */ 
-
-        text_onscreen(src); 
+             text_onscreen(src); 
 
         //imshow("Circle Detection", src);
         imshow("Gaussian Blur", gaussian_result);
