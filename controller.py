56f321a2e0f30e1ecba5e41509e6cd6fd78be675
srli <sophia.li@students.olin.edu>
2014-10-31 13:46:03 -0400

    added some files

--- controller.py
+++ controller.py
@@ -1,8 +1,8 @@
-import rospy
-from math import *
-from std_msgs.msg import String
-from geometry_msgs.msg import Twist, Vector3
-from sensor_msgs.msg import LaserScan
+#import rospy
+#from math import *
+#from std_msgs.msg import String
+#from geometry_msgs.msg import Twist, Vector3
+#from sensor_msgs.msg import LaserScan
 import cv2
 import numpy as np
 from cv2 import cv
@@ -11,15 +11,51 @@ def track_color():
 	ret, frame = cap.read()
 	cimg = frame
 
-	imgHSV = cv2.cvtColor(cimg, cv.CV_BGR2HSV)
-	imgThreshed = cv2.inRange(imgHSV, np.array((60,70,70)), np.array((120,255,255)))
+	gaussian_images = []
 
-	gaussian_res = cv2.GaussianBlur(imgThreshed, (9,9), 2, 2)
+	img_HSV = cv2.cvtColor(cimg, cv.CV_BGR2HSV)
+	
+	red_Threshed = cv2.inRange(img_HSV, np.array((0,200,180)), np.array((10,255,255)))
+	red_gaussian = cv2.GaussianBlur(red_Threshed, (9,9), 2, 2)
+
+	blue_Threshed = cv2.inRange(img_HSV, np.array((60,70,70)), np.array((120,255,255)))
+	blue_gaussian = cv2.GaussianBlur(blue_Threshed, (9,9), 2, 2)
+
+	green_Threshed = cv2.inRange(img_HSV, np.array((60,70,70)), np.array((120,255,255)))
+	green_gaussian = cv2.GaussianBlur(green_Threshed, (9,9), 2, 2)
+
+	pink_Threshed = cv2.inRange(img_HSV, np.array((60,70,70)), np.array((120,255,255)))
+	pink_gaussian = cv2.GaussianBlur(pink_Threshed, (9,9), 2, 2)
+
+	yellow_Threshed = cv2.inRange(img_HSV, np.array((60,70,70)), np.array((120,255,255)))
+	yellow_gaussian = cv2.GaussianBlur(yellow_Threshed, (9,9), 2, 2)
+
+	total_threshed = red_Threshed + blue_Threshed + green_Threshed + pink_Threshed + yellow_Threshed
+	total_gaussian = red_gaussian + blue_gaussian + green_gaussian + pink_gaussian + yellow_gaussian
 
 	#cv2.imshow("Gaussian Blur", gaussian_res)
-	cv2.imshow("HSV Image", imgHSV)
+	cv2.imshow("HSV Image", img_HSV)
+	cv2.imshow("threshed", total_threshed)
 	c = cv2.waitKey(1)
-	return gaussian_res
+
+	gaussian_images.append(red_gaussian)
+	gaussian_images.append(blue_gaussian)
+	gaussian_images.append(green_gaussian)
+	gaussian_images.append(pink_gaussian)
+	gaussian_images.append(yellow_gaussian)
+
+	return gaussian_images
+
+def find_location(img):
+	"""
+	pass in Image of each finger/plam
+	using opencv function, pass out x,y location of point
+	"""
+	pass
+
+
+
+
 
 def find_ratios(img):
 	circles = cv2.HoughCircles(img, cv.CV_HOUGH_GRADIENT,1,200,
@@ -57,22 +93,52 @@ def find_ratios(img):
 	#ratio = float(num_bright_pixels) / float(rows*cols)
 	#print hist[255]
 
-def find_command(x,y):
-	a = atan2(y,x)
-	dist = squrt(x**2 + y**2)
-	return .2*speed, a
 
-def control_robot(x,a):
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    msg = Twist (Vector3 (x, 0, 0), Vector3 (0, 0, a))
+
+def identify_command(thumb_loc, index_loc, middle_loc, ring_loc, pinky_loc):
+	pass #something something do stuff with these locations
+	return command
+	# a = atan2(y,x)
+	# dist = squrt(x**2 + y**2)
+	# return .2*speed, a
+
+def control_robot(command):
+	pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+	if command == "back":    
+	    msg = Twist (Vector3 (x, 0, 0), Vector3 (0, 0, a))
+	elif command == "forward":
+		msg = Twist (Vector3 (x, 0, 0), Vector3 (0, 0, a))
+	elif command == "left":
+		msg = Twist (Vector3 (x, 0, 0), Vector3 (0, 0, a))
+	elif command == "right":
+		msg = Twist (Vector3 (x, 0, 0), Vector3 (0, 0, a))
+
+
 
 if __name__ == "__main__":
 	#rospy.init_node('neato_controller', anonymous=True)
 	cap = cv2.VideoCapture(0)
 
-	cv2.namedWindow("Gaussian Blur")
+	#cv2.namedWindow("Gaussian Blur")
 	cv2.namedWindow("HSV Image")
 
+
 	while True:
-		(x,y) = find_ratios(track_color())
-		control_robot(find_command(x,y))
+		gaussian_images = track_color()
+		thumb 	= 	gaussian_images[0]
+		index 	= 	gaussian_images[1]
+		middle 	= 	gaussian_images[2]
+		ring 	= 	gaussian_images[3]
+		pinky 	= 	gaussian_images[4]
+
+		thumb_loc = find_location(thumb)
+		index_loc = find_location(index)
+		middle_loc = find_location(middle)
+		ring_loc = find_location(ring)
+		pinky_loc = find_location(pinky)
+
+		command = identify_command(thumb_loc, index_loc, middle_loc, ring_loc, pinky_loc)
+		control_robot(command)
+
+		#(x,y) = find_ratios(track_color())
+		#control_robot(find_command(x,y))
--- src/followcolor.cpp
+++ src/followcolor.cpp
@@ -19,13 +19,66 @@ void text_onscreen(Mat src){
   putText(src, text, textOrg, fontFace, fontScale, Scalar(0,0,0,255), thickness,8);
 }
 
+int histograms(Mat inputimg){
+  /// Separate the image in 2 places, black, white
+  vector<Mat> bw_planes;
+  split(inputimg, bw_planes);
+
+  /// Establish the number of bins
+  int histSize = 256;
+
+  /// Set the ranges ( for B,W) )
+  float range[] = { 0, 256} ;
+  const float* histRange = { range };
+
+  bool uniform = true; bool accumulate = false;
+
+  Mat b_hist, w_hist;
+
+  /// Compute the histograms:
+  calcHist( &bw_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
+  calcHist( &bw_planes[1], 1, 0, Mat(), w_hist, 1, &histSize, &histRange, uniform, accumulate );
+  //calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+
+  // Draw the histograms for B, G and R
+  int hist_w = 512; int hist_h = 400;
+  int bin_w = cvRound( (double) hist_w/histSize );
+
+  Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+
+  /// Normalize the result to [ 0, histImage.rows ]
+  normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+  normalize(w_hist, w_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+  //normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+/*
+  /// Draw for each channel
+  for( int i = 1; i < histSize; i++ )
+  {
+      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+                       Scalar( 255, 0, 0), 2, 8, 0  );
+      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(w_hist.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(w_hist.at<float>(i)) ),
+                       Scalar( 0, 255, 0), 2, 8, 0  );
+  }
+
+  /// Display
+  namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
+  imshow("calcHist Demo", histImage );
+*/
+
+  return 0;
+}
+
+
 int main(int argc,char *argv[])
 {
     int c, key;
     double pi = 3.1415926535897;    
-    Mat src, gray, gaussian_result;
-    Mat imgHSV, imgThreshed, imgPalm, imgThumb, imgIndex;
-    
+    Mat src, gray, gaussian_result, imgHSV;
+    Mat imgPalm, imgThumb, imgIndex;
+    Mat GimgPalm, GimgThumb, GimgIndex;
+
     IplImage* color_img;
     CvCapture* cv_cap = cvCaptureFromCAM(0);
     //cvNamedWindow("Circle Detection",0); // create window
@@ -39,18 +92,79 @@ int main(int argc,char *argv[])
 
         //Changing color image to HSV for color filtering
         cvtColor(src, imgHSV, CV_BGR2HSV);
-        inRange(imgHSV, Scalar(60, 70, 70), Scalar(120, 255, 255), imgPalm);
+        cvtColor(src, gray, CV_BGR2GRAY);
+        //imgPalm = imgHSV;
+        inRange(imgHSV, Scalar(0, 1, 1), Scalar(255, 255, 255), imgPalm);
         inRange(imgHSV, Scalar(50, 200, 200), Scalar(70, 255, 255), imgThumb);
-        //inRange(imgHSV, Scalar(60, 70, 70), Scalar(120, 255, 255), imgIndex);
+        inRange(imgHSV, Scalar(60, 70, 70), Scalar(120, 255, 255), imgIndex); //now we have separate mat images for each part we want to track
         
-/*        // Reduce the noise so we avoid false circle detection
-        GaussianBlur( imgThreshed, gaussian_result, Size(9, 9), 2, 2 );
-             text_onscreen(src); 
-*/
+
+        // Reduce the noise so we avoid false circle detection
+        GaussianBlur( imgPalm, GimgPalm, Size(9, 9), 2, 2 );
+        GaussianBlur( imgThumb, GimgThumb, Size(9, 9), 2, 2 );
+        GaussianBlur( imgIndex, GimgIndex, Size(9, 9), 2, 2 );
+
+        vector<Vec3f> circles;
+
+        // Apply the Hough Transform to find the circles
+        HoughCircles(GimgPalm, circles, CV_HOUGH_GRADIENT, 1, 30, 200, 30, 20, 0 );
+        HoughCircles(GimgThumb, circles, CV_HOUGH_GRADIENT, 1, 30, 200, 30, 20, 0 );
+        HoughCircles(GimgIndex, circles, CV_HOUGH_GRADIENT, 1, 30, 200, 30, 20, 0 );
+        
+        cout << circles.size() << endl;
+
+        histograms(gray);
+
+
+       /* if (circles.size() != 0){
+            
+            // Draw the circles detected
+            //for( size_t i = 0; i < circles.size(); i++ )
+            for( size_t i = 0; i < circles.size(); i++ ){
+                Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+                int radius = cvRound(circles[i][2]);
+
+                //Drawing each circle
+                circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );//center
+                circle( src, center, radius, Scalar(0,0,255), 3, 8, 0 );//circumference
+
+                //Drawing lines relative to center
+                Point midpoint(center.x, center_screen.y);
+                line(src, center_screen, center, Scalar(255,0,0), 1, 8, 0);//center to center
+                line(src, center_screen, midpoint, Scalar(255,0,0), 1, 8, 0);//y
+                line(src, midpoint, center, Scalar(255,0,0), 1, 8, 0);//x
+                
+                //cout << "center : " << center << "\nradius : " << radius << endl;
+                
+                double x_distance = center.x - center_screen.x;
+                double y_distance = center.y - center_screen.y;
+
+                double distance = 126.964*exp(-0.0216358 * radius);
+                double pan_angle = tan(x_distance/distance);
+                double tilt_angle = tan(y_distance/distance);
+
+                printf("");
+
+                key = cvWaitKey(100);
+
+                if(key == 97){
+                //angle = fmod(angle, pi);
+                printf("Robot Coordinates\n");
+                cout << "radius:  " << radius << endl;
+                cout << "pan:  " << pan_angle << endl;
+                cout << "tilt:  " << tilt_angle << endl;
+                cout << "distance:  " << distance << endl;
+                cout << "-------------" << endl;
+            }
+        }
+    }*/
+
+        //text_onscreen(src); 
+
         //imshow("Circle Detection", src);
-        imshow("Gaussian Blur", imgPalm);
-        imshow("Gaussian Blur", imgThumb);
-        imshow("HSV Image", imgHSV);
+        imshow("Src", src);
+      //  imshow("Gaussian Blur", imgThumb);
+        imshow("HSV Image", imgIndex);
 
         c = cvWaitKey(10); // wait 10 ms or for key stroke
         if(c == 27){
